9e412af9e03f75e53c5048131d2c8175d65db716
carmenarrabali@gmail.com <carmenarrabali@gmail.com>
2021-06-24 18:29:26 +0200

    Update

---
+++ src/main/java/PatronCadenaMando/Inhalers.java
@@ -0,0 +1,33 @@
+package PatronCadenaMando;
+
+import com.google.gson.stream.JsonReader;
+
+import java.io.*;
+
+public class Inhalers {
+    private static final String INHALERS_TAGNAME = "inhalers";
+    private static final String NAME_FIELD_TAGNAME = "name";
+    private static final String IMAGE_FIELD_TAGNAME = "image";
+    private static final String FIELD_SEPARATOR = ";";
+
+    protected String readObjectEntry(String jsonFileName) throws IOException {
+        InputStream usersIS = new FileInputStream(new File(jsonFileName));
+        JsonReader reader = new JsonReader(new InputStreamReader(usersIS, "UTF-8"));
+
+        String inhaName = null;
+        String inhaImage = null;
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals(NAME_FIELD_TAGNAME)) {
+                inhaName = reader.nextString();
+            } else if (name.equals(IMAGE_FIELD_TAGNAME)) {
+                inhaImage = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        return inhaName + FIELD_SEPARATOR + inhaImage;
+    }
+
+
+}
